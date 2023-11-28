be0c3433c4143af09db654c56cc1356d98ae47fa
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2023-11-28 16:08:28 +0100

    setup gui panels

--- src/Client/GameGUI.java
+++ src/Client/GameGUI.java
@@ -27,10 +27,14 @@ public class GameGUI extends JFrame {
     JButton answerD;
 
     JPanel waitPanel;
+    JPanel waitPanelNorth;
+    JPanel waitPanelSouth;
     JLabel waitLabel;
-    JButton playBtn; //default disabled
+    JButton waitPlayBtn; //default disabled
 
     JPanel finalPanel;
+    JPanel finalSouth;
+    JPanel finalNorth;
     JLabel playerPoints1;
     JLabel playerPoints2;
     JLabel winnerLabel;
@@ -38,13 +42,17 @@ public class GameGUI extends JFrame {
     JButton quitGameBtn;
 
     public GameGUI() {
-        setStartPanel();
+        setStartPanel(); //**
         setCategoryPanel();
+        setQuestionPanel(); //*****
+        setWaitPanel();
+        setFinalPanel();
 
+        this.add(startPanel);
+//        this.add(startPanel);
+//        this.add(categoryPanel);
+//        this.add(waitPanel);
 
-//        setQuestionPanel();
-//        setWaitPanel();
-//        setFinalPanel();
 
         this.setSize(800, 600);
         this.setResizable(false);
@@ -53,20 +61,22 @@ public class GameGUI extends JFrame {
         this.setVisible(true);
     }
 
-
     public void setStartPanel() {
-        startPanel = new JPanel();
-        startPanel.setLayout(new GridLayout(4, 1));
+        startPanel = new JPanel(new GridLayout(4, 1));
         startPanel.setBorder(new EmptyBorder(100, 120, 0, 120));
 
         gameNameLabel = new JLabel("QUIZKAMPEN");
         gameNameLabel.setFont(new Font("Tahoma", Font.BOLD, 45));
+        gameNameLabel.setHorizontalAlignment(JLabel.CENTER);
 
         namePromptText = new JLabel("Vänligen ange ett namn för att starta spelet");
         namePromptText.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        namePromptText.setHorizontalAlignment(JLabel.CENTER);
 
         nameField = new JTextField();
         nameField.setFont(new Font("Tahoma", Font.BOLD, 25));
+        nameField.setHorizontalAlignment(JTextField.CENTER);
+       nameField.setPreferredSize(new Dimension(300,50));
 
         //actionlistener
         submitButton = new JButton("Spela");
@@ -75,7 +85,7 @@ public class GameGUI extends JFrame {
         startPanel.add(namePromptText);
         startPanel.add(nameField);
         startPanel.add(submitButton);
-
+        pack();
     }
 
     public void setCategoryPanel() {
@@ -116,12 +126,55 @@ public class GameGUI extends JFrame {
     }
 
     public void setWaitPanel() {
+        waitPanel = new JPanel(new GridLayout(2,1));
+        waitPanelNorth = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        waitPanelSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
+
+        waitLabel = new JLabel("Väntar på motspelare...");
+        waitLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+        waitLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        waitPlayBtn = new JButton("Spela");
+        waitPlayBtn.setEnabled(false); //false tills får spela
+        waitPlayBtn.setPreferredSize(new Dimension(300,100));
+        waitPlayBtn.setHorizontalAlignment(JButton.CENTER);
+
+        waitPanelNorth.add(waitLabel);
+        waitPanelSouth.add(waitPlayBtn);
+
+        waitPanel.add(waitPanelNorth);
+        waitPanel.add(waitPanelSouth);
     }
 
     public void setFinalPanel() {
+        finalPanel = new JPanel(new GridLayout(2,1));
+        finalNorth = new JPanel(new GridLayout(3,1));
+        finalSouth = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
+        playerPoints1 = new JLabel("Player1 poäng");
+        playerPoints2 = new JLabel("Player2 poäng");
+        playerPoints1.setFont(new Font("Tahoma", Font.BOLD, 30));
+        playerPoints2.setFont(new Font("Tahoma", Font.BOLD, 30));
+        playerPoints1.setHorizontalAlignment(JLabel.CENTER);
+        playerPoints2.setHorizontalAlignment(JLabel.CENTER);
 
+        winnerLabel = new JLabel("Vinnaren är : ");
+        winnerLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+        winnerLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        playAgainBtn = new JButton("Spela igen");
+        quitGameBtn = new JButton("Avsluta");
+
+        finalNorth.add(playerPoints1);
+        finalNorth.add(playerPoints2);
+        finalNorth.add(winnerLabel);
+
+        finalSouth.add(playAgainBtn);
+        finalSouth.add(quitGameBtn);
+
+        finalPanel.add(finalNorth);
+        finalPanel.add(finalSouth);
     }
 
 
