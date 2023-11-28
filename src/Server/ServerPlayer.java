e39fa1600a9238ea3c3b6d0b83fb9ca726e6a4c8
Kareem D <drammeh@msn.com>
2023-11-28 14:07:45 +0100

    ServerPlayer method addded

--- src/Server/ServerPlayer.java
+++ src/Server/ServerPlayer.java
@@ -1,6 +1,45 @@
 package Server;
 
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ServerPlayer {
+    ServerPlayer opponent;
+    Socket socket;
+
+
+    public ServerPlayer(Socket socket) throws IOException {
+        this.socket = socket;
+    try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+         PrintWriter output = new PrintWriter(socket.getOutputStream(), true)
+    ) {
+
+
+
+
+
+
+
+
+    }catch (IIOException e) {
+        e.printStackTrace();
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
 
     //egenskaper för en spelare
 }
