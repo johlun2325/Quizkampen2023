fdf6fb221e29517f91deea01380c469ad027b3f8
Karl Engwall <karlengwall@gmail.com>
2023-11-28 14:33:57 +0100

    Added ServerListener

--- src/Server/ServerListener.java
+++ src/Server/ServerListener.java
@@ -1,6 +1,24 @@
 package Server;
 
-public class ServerListener {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class ServerListener {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(44456);
+
+        while (true) {
+
+            try {
+                ServerPlayer player1 = new ServerPlayer(ss.accept(), "Player1");
+                ServerPlayer player2 = new ServerPlayer(ss.accept(),"Player2");
+                ServerGame game = new ServerGame(player1, player2);
+                game.start();
+
+            } finally {
+                ss.close();
+            }
+        }
+    }
     //accepta anslutningar och starta game
 }
