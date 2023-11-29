44508187e343ace5d377c9b966b3163e886be4bd
Kalle Johansson <kalle.johansson@hotmail.com>
2023-11-29 19:54:36 +0100

    Added first turnbased logic and name saving for both players

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -43,15 +43,25 @@ public class Client {
                 fromServer = in.readObject();
                 System.out.println("Från server" + fromServer);
 
-                if (fromServer.equals("Ansluten")) {
-
+                if (fromServer.equals("AnslutenPlayer1")) {
                     while (gui.getNameFromGui().isEmpty()) {
                         System.out.println(gui.getNameFromGui());
                         Thread.sleep(5000);
                     }
-                    out.println("Name:" + gui.name); //Ev. out.flush() efter
+                    out.println("NamePlayer1:" + gui.name); //Ev. out.flush() efter
                     out.flush();
                     System.out.println("Name sent " + gui.name);
+
+
+                } else if (fromServer.equals("AnslutenPlayer2")) {
+                    while (gui.getNameFromGui().isEmpty()) {
+                        System.out.println(gui.getNameFromGui());
+                        Thread.sleep(5000);
+                    }
+                    out.println("NamePlayer2:" + gui.name); //Ev. out.flush() efter
+                    out.flush();
+                    System.out.println("Name sent " + gui.name);
+
 
                 } else if (fromServer.equals("Category")) {
 
@@ -59,15 +69,16 @@ public class Client {
 
                     String chosenCategory = null;
 
-                    while(chosenCategory == null || chosenCategory.isEmpty()){
+                    while (chosenCategory == null || chosenCategory.isEmpty()) {
                         chosenCategory = gui.getChosenCategoryFromGui();
-                        try{
+                        try {
                             Thread.sleep(1000);
-                        }catch (InterruptedException e){
+                        } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
 
-                    System.out.println("Client: i kategori");}
+                        System.out.println("Client: i kategori");
+                    }
                     out.println(chosenCategory);                   // Skickar  ChosenCategory till Servern
                     out.flush();
                     System.out.println("Skriver ut kategori: " + chosenCategory);
@@ -76,27 +87,27 @@ public class Client {
                 } else if (fromServer.equals("Wait")) {
                     gui.showWaitPanel();
 
-                //} else if (fromServer.equals("History")) {
+                    //} else if (fromServer.equals("History")) {
                 } else if (fromServer instanceof List) {
                     gui.showQuestionPanel();
                     System.out.println("nu är vi i questionpanel");
-                        List<?> list = (List<?>) fromServer;
-                        if (!list.isEmpty() && list.get(0) instanceof Question) {
-                            List<Question> questions = (List<Question>) list;
-                            System.out.println("Client inne i if fromServer instanceof List, Questions mottagna");
+                    List<?> list = (List<?>) fromServer;
+                    if (!list.isEmpty() && list.get(0) instanceof Question) {
+                        List<Question> questions = (List<Question>) list;
+                        System.out.println("Client inne i if fromServer instanceof List, Questions mottagna");
 
-                            for (int i = 0; i < 2; i++) {
-                                if (questionAnswered)
+                        for (int i = 0; i < 2; i++) {
+                            if (questionAnswered)
                                 gui.questionLabel.setText(questions.get(i).getQuestion());
-                                gui.answerA.setText(questions.get(0).getCorrectAnswer());
-                                gui.answerB.setText(questions.get(0).getWrongAnswer1());
-                                gui.answerC.setText(questions.get(0).getWrongAnswer2());
-                                gui.answerD.setText(questions.get(0).getWrongAnswer3());
-                                questionAnswered = false;
-                            }
+                            gui.answerA.setText(questions.get(0).getCorrectAnswer());
+                            gui.answerB.setText(questions.get(0).getWrongAnswer1());
+                            gui.answerC.setText(questions.get(0).getWrongAnswer2());
+                            gui.answerD.setText(questions.get(0).getWrongAnswer3());
+                            questionAnswered = false;
                         }
+                    }
 
-                        out.println("Här borde poäng skickas till Servern");
+                    out.println("Här borde poäng skickas till Servern");
 
 
                     //här läser vi väl in frågorna?
@@ -105,16 +116,17 @@ public class Client {
                     gui.showFinalPanel();
 
 
-                }}
-            } catch (IOException ex) {
+                }
+            }
+        } catch (IOException ex) {
             throw new RuntimeException(ex);
         } catch (ClassNotFoundException ex) {
             throw new RuntimeException(ex);
-       // } catch (InterruptedException ex) {
-          //  throw new RuntimeException(ex);
+            // } catch (InterruptedException ex) {
+            //  throw new RuntimeException(ex);
 
 
-    } catch (Exception e) {  //FINALLY
+        } catch (Exception e) {  //FINALLY
             e.printStackTrace();
 
         }
--- src/Server/ServerGame.java
+++ src/Server/ServerGame.java
@@ -11,7 +11,7 @@ public class ServerGame extends Thread {
     ServerPlayer player2;
     ServerPlayer currentPlayer;
     Database db = new Database();
-    String name;
+
 
     public ServerGame(ServerPlayer player1, ServerPlayer player2) {
         this.player1 = player1;
@@ -24,49 +24,46 @@ public class ServerGame extends Thread {
     public void run() {
 
         try {
-            player1.sendString("Ansluten");
-            player2.sendString("Ansluten");
+            player1.sendString("AnslutenPlayer1");
+            player2.sendString("AnslutenPlayer2");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
 
-        //player1.sendString("Category");
-        //currentPlayer = player1;
-        
-        
         String inputStringFromPlayer1 = "";
-        String inputStringFromPlayer2 = "";
 
         while (true) {
             inputStringFromPlayer1 = currentPlayer.receiveString();
-            //inputStringFromPlayer2 = currentPlayer.getOpponent().receiveString();
 
-            // debugg JOptionPane.showMessageDialog(null, inputStringFromClient + "FRÅN SERVERGAME");
-            System.out.println("RAD33" + inputStringFromPlayer1);
-
-            if (inputStringFromPlayer1.startsWith("Name:")) {
-                name = inputStringFromPlayer1.substring(5).trim(); //Spara namn
-                currentPlayer.setPlayer(name);
-                System.out.println(name + "Här är namnet spelare 1");
-
-                //name = inputStringFromPlayer2.substring(5).trim();
-                currentPlayer.getOpponent().setPlayer(name);
-                System.out.println(name + "Här är namnet spelare 2");
+            if (inputStringFromPlayer1.startsWith("NamePlayer1:")) {
+                String name = inputStringFromPlayer1.substring(12).trim(); //Spara namn
+                player1.setPlayer(name);
+                System.out.println(name + " Här är namnet för spelare 1");
 
                 try {
                     currentPlayer.sendString("Category");
-                    currentPlayer.getOpponent().sendString("Wait");
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                //debugg JOptionPane.showMessageDialog(null, "Name received " + name);
+                currentPlayer = currentPlayer.getOpponent();  //BYTER CURRENT-PLAYER
 
+            } else if (inputStringFromPlayer1.startsWith("NamePlayer2:")) {
+                String name = inputStringFromPlayer1.substring(12).trim(); //Spara namn
+                player2.setPlayer(name);
+                System.out.println(name + " Här är namnet för spelare 2");
+
+                try {
+                    currentPlayer.sendString("Wait");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                currentPlayer = currentPlayer.getOpponent(); //BYTER CURRENT-PLAYER
 
             } else if (inputStringFromPlayer1.trim().equalsIgnoreCase("History")) {
                 System.out.println("Inuti else if equals(History)");
                 player1.sendObject(db.getHistory());
-               // player2.sendObject(db.getHistory());
+                player2.sendObject(db.getHistory());
 
             } else if (inputStringFromPlayer1.equals("Music")) {
                 player1.sendObject(db.getMusic());
@@ -83,11 +80,7 @@ public class ServerGame extends Thread {
         //currentPlayer = currentPlayer.getOpponent();  //BYTER SPELARE
     }
 
-    public List<Question> getListOfQuestions (List<Question> listFromDatabase){
+    public List<Question> getListOfQuestions(List<Question> listFromDatabase) {
         return listFromDatabase;
     }
 }
-
-
-//ett spel emd båda spelarna
-
